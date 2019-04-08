8e62690ea098bc9f88c61874774d44f1942f6382
dutradev <dutradev.it@gmail.com>
2019-04-08 23:39:31 +0700

    create layout

---
+++ app/src/main/java/com/ductran/image/ImageData.java
@@ -0,0 +1,22 @@
+package com.ductran.image;
+
+public class ImageData {
+    private String imageContent;
+    private String imageUrl;
+
+    public String getImageContent() {
+        return imageContent;
+    }
+
+    public void setImageContent(String imageContent) {
+        this.imageContent = imageContent;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
