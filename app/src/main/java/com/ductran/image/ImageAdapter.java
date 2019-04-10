1de18863438ff00f345c6e9f792db19c1299ff58
dutradev <dutradev.it@gmail.com>
2019-04-10 07:23:04 +0700

    load image

---
+++ app/src/main/java/com/ductran/image/ImageAdapter.java
@@ -0,0 +1,63 @@
+package com.ductran.image;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<ImageData> imageData;
+
+    public ImageAdapter(Context context, ArrayList<ImageData> imageData) {
+        this.context = context;
+        this.imageData = imageData;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ImageData item = imageData.get(position);
+        holder.tvContent.setText(item.getImageContent());
+
+        Glide.with(context)
+                .load(item.getImageUrl())
+                .apply(new RequestOptions().override(120, 60).centerCrop().placeholder(R.drawable.ic_launcher_background)
+                )
+                .into(holder.img);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageData.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvContent;
+        ImageView img;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            tvContent = itemView.findViewById(R.id.tv_content);
+            img = itemView.findViewById(R.id.img);
+        }
+    }
+}
--- app/src/main/java/com/ductran/image/MainActivity.java
+++ app/src/main/java/com/ductran/image/MainActivity.java
@@ -2,12 +2,255 @@ package com.ductran.image;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rclImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        initData();
     }
+
+    private void initViews() {
+        rclImage = findViewById(R.id.rclImage);
+        rclImage.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rclImage.setLayoutManager(layoutManager);
+
+    }
+
+    private void initData() {
+        ArrayList<ImageData> imageList = prepareData();
+        ImageAdapter adapter = new ImageAdapter(this, imageList);
+        rclImage.setAdapter(adapter);
+    }
+
+    private ArrayList<ImageData> prepareData() {
+
+        ArrayList<ImageData> imageList = new ArrayList<>();
+        for (int i = 0; i < android_version_names.length; i++) {
+            ImageData imageData = new ImageData();
+            imageData.setImageContent(android_version_names[i]);
+            imageData.setImageUrl(android_image_urls[i]);
+            imageList.add(imageData);
+        }
+        return imageList;
+    }
+
+    private final String android_version_names[] = {
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow",
+    };
+
+    private final String android_image_urls[] = {
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png",
+    };
+
 }
